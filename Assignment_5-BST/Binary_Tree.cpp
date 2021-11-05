c16cd37d4060d5a9cef3a864266c7fce80560b1e
AbhishekJadhav2002 <www.abhishek3jadhav@gmail.com>
2021-11-05 23:51:07 +0530

    first commit

---
+++ Assignment_5-BST/Binary_Tree.cpp
@@ -0,0 +1,123 @@
+/*
+============================================================================
+ Name : Binary_Tree.cpp
+ Author : 23232_Abhishek Jadhav
+ Version : 1.0
+ Copyright : Your copyright notice
+ Description : DSAL Lab Assignment No. 5
+============================================================================
+*/
+
+#include <bits/stdc++.h>
+#include "Binary_Tree_Header.h"
+#include <vector>
+#include <exception>
+using namespace std;
+
+int main()
+{
+    Binary_Tree<int> BT;
+    cout<<"\t\t\t\t\t\t\t\t---------- Binary Seach Tree ----------";
+    Functions_after_switch :
+    {
+        cout<<"\n\nFuntions -\n\t1. Insert Array \t2. Delete Element\t3. Search\t4. Display Tree \t5. Mirror Convert\t6. Copy Tree\t7. Leaf Nodes\t8. LevelWise Traversal\n\t\t\t\t\t\t\t\t\tEnter Choice : ";
+        int choice;
+        cin>>choice;
+        switch(choice)
+        {
+            case 1:
+            {
+                // int input;
+                // vector<int> v;
+                // cout<<"\nEnter -1 to stop entering elements...\nEnter Elements : ";
+                // bool app = true;
+                // while(app)
+                // {
+                //     cin>>input;
+                //     if(input == -1){app = false;}
+                //     else{v.push_back(input);}
+                // }
+                // cout<<"\n[ Elements entered in tree ]";
+                int size = 5;
+                int arr[size];
+                for(int i = 0; i < size; i++){cin>>arr[i];}
+                BT.InsertArray(arr, size);
+                // BT.InsertArray(&v.front(), v.size());
+                cout<<"\nBinary Tree built is : ";
+                BT.InorderRec(BT.GetRoot());
+                goto Functions_after_switch;
+            }      
+            case 2:
+            {
+                cout<<"\nTree : ";BT.InorderRec(BT.GetRoot());
+                int dlt_element;
+                cout<<"\nEnter element to be deleted from above tree : ";
+                cin>>dlt_element;
+                BT.DeleteNode(BT.GetRoot(), dlt_element);
+                cout<<"\n[ "<<dlt_element<<" deleted from tree ]";
+                goto Functions_after_switch;
+            }
+            case 3:
+            {
+                int search_key;
+                cout<<"\nEnter key to be searches in tree : ";
+                cin>>search_key;
+                BT.isPresent(search_key);
+                goto Functions_after_switch;
+            }
+            case 4:
+            {
+                cout<<"\nDisplay Tree -\n\t1.Inorder\t2.Preorder\t3.Postorder\n\t\tSelect View : ";
+                int view;
+                cin>>view;
+                switch(view)
+                {
+                    case 1:
+                    {
+                        cout<<"\nInorder Traversal(ascending) : "; BT.InorderRec(BT.GetRoot());
+                        goto Functions_after_switch;
+                    }
+                    case 2:
+                    {
+                        cout<<"\nPreorder Traversal : "; BT.PreorderRec(BT.GetRoot());
+                        goto Functions_after_switch;
+                    }
+                    case 3:
+                    {
+                        cout<<"\nPostorder Traversal : "; BT.PostorderRec(BT.GetRoot());
+                        goto Functions_after_switch;
+                    }
+                }
+            }
+            case 5:
+            {
+                cout<<"\nBinary Tree before mirroring : ";BT.InorderRec(BT.GetRoot());
+                cout<<"\nBinary Tree after mirroring : ";BT.MirrorImage(BT.GetRoot());BT.InorderRec(BT.GetRoot());
+                cout<<"\n[ Don't forget to select Mirroring Tree option again ]";
+                goto Functions_after_switch;
+            }
+            case 6:
+            {
+                cout<<"\nCoping Tree1 to Tree2 -> Tree1 : ";BT.InorderRec(BT.GetRoot());
+                Node<int>* tree2;
+                cout<<"\n                           Tree2 : ";BT.copyTree(tree2, BT.GetRoot());
+                goto Functions_after_switch;
+            }
+            case 7:
+            {
+                cout<<"\nDisplying Leaf Nodes : ";BT.DisplayLeafNodes(BT.GetRoot());
+                goto Functions_after_switch;
+            }
+            case 8:
+            {
+                cout<<"\nLevel Wise Traversal : ";BT.DisplayLevelWise(BT.GetRoot());
+                goto Functions_after_switch;
+            }
+            default :
+            {
+                cout<<"\n\n\t\t----Program Terminated----\n";
+                return 0;
+            }
+        }
+    }
+}
---
+++ Assignment_5-BST/Binary_Tree_Header.h
@@ -0,0 +1,359 @@
+/*
+============================================================================
+ Name : Binary_Tree_Header.h
+ Author : 23232_Abhishek Jadhav
+ Version : 1.0
+ Copyright : Your copyright notice
+ Description : DSAL Lab Assignment No. 5
+============================================================================
+*/
+#ifndef _Binary_Tree_H_
+#define _Binary_Tree_H_
+#include <bits/stdc++.h>
+#include <queue>
+#define max(a, b) (((a) > (b)) ? (a) : (b))  //defining user defined macro preprocessor function
+using namespace std;
+
+template<typename B>
+struct Node
+{
+    B data;
+    Node<B>* left;
+    Node<B>* right;
+};
+
+template<typename B>
+class Binary_Tree
+{
+    private :
+        struct Node<B>* root;
+    public :
+        Binary_Tree()
+        {
+            root = NULL;
+        }
+        Node<B>* Makenode(B x);
+        Node<B>* GetRoot();
+        void Insert(B x);
+        void InsertArray(B *arr, int size);
+        bool isPresent(B k);
+        // Node<B>* Search(B k);
+        Node<B>* GetNodeOf(B k);
+        Node<B>* DeleteNode(Node<B>* root, B k);
+        Node<B>* findMin(Node<B>* root);
+        void InorderRec(Node<B>*);
+		void PreorderRec(Node<B>*);
+		void PostorderRec(Node<B>*);
+        int Height(Node<B>*);
+        void MirrorImage(Node<B>*);
+        void copyTree(Node<B>* tree1, Node<B>* tree2);
+        void DisplayLeafNodes(Node<B>*);
+        void DisplayLevelWise(Node<B>*);
+};
+
+template<typename B>
+Node<B>* Binary_Tree<B> :: Makenode(B x)  //creates one node with data as parameter passed
+{
+    Node<B>* newNode = new struct Node<B>();
+    newNode->data = x;
+    newNode->left = NULL;
+    newNode->right = NULL;
+    return newNode;
+}
+
+template<typename B>
+Node<B>* Binary_Tree<B> :: GetRoot()
+{
+    return root;
+}
+
+template<typename B>
+void Binary_Tree<B> :: Insert(B x)
+{
+    if(root == NULL)
+    {
+        root = Makenode(x);
+        return;
+    }
+    else
+    {
+        Node<B>* P = root;
+        Node<B>* Q = root;
+        while(Q != NULL && x != P->data)
+        {
+            P = Q;
+            if(x < P->data)
+            {
+                Q = P->left;
+            }
+            else if(x > P->data)
+            {
+                Q = P->right;
+            }
+        }
+        if(x == P->data)
+        {
+            cout<<"\n"<<x<<" is already present, Duplicates are not allowed";
+            return;
+        }
+        if(x < P->data)
+        {
+            P->left = Makenode(x);
+            return;
+        }
+        else
+        {
+            P->right = Makenode(x);
+            return;
+        }
+    }
+}
+
+template<typename B>
+void Binary_Tree<B> :: InsertArray(B *arr, int size)
+{
+    for(int i = 0; i < size; i++)
+    {
+        Binary_Tree :: Insert(*(arr + i));
+    }
+}
+
+template<typename B>
+bool Binary_Tree<B> :: isPresent(B k)
+{
+    if(root == NULL)
+    {
+        cout<<"\n[ Empty Tree ]";
+        return false;
+    }
+    else
+    {
+        if(Binary_Tree :: GetNodeOf(k) != NULL)
+        {
+            cout<<"\n"<<k<<" is present in the tree";
+            return true;
+        }
+        else
+        {
+            // cout<<"\nElement "<<k<<" not present in the tree";
+            return false;
+        }
+    }
+}
+
+// template<typename B>
+// Node<B>* Binary_Tree<B> :: Search(B k)
+// {
+//     Node<B>* p = root;
+//     while(p)
+//     {
+//         if(k == p->data)
+//         {
+//             return p;
+//         }
+//         else if(k < p->data)
+//         {
+//             p = p->left;
+//         }
+//         else
+//         {
+//             p = p->right;
+//         }
+//     }
+//     return NULL;
+// }
+
+template<typename B>
+Node<B>* Binary_Tree<B> :: GetNodeOf(B k)
+{
+    if(root == NULL)
+    {
+        root = Makenode(k);
+        cout<<"\n[ Tree is empty, creating root node ]";
+        return root;
+    }
+    else
+    {
+        Node<B>* p = root;
+        while(p)
+        {
+            if(k == p->data)
+            {
+                return p;
+            }
+            else if(k < p->data)
+            {
+                p = p->left;
+            }
+            else
+            {
+                p = p->right;
+            }
+        }
+        cout<<"\n[ Element "<<k<<" is not present in the tree returning NULL ]";
+        return NULL;
+    }
+}
+
+template<typename B>
+Node<B>* Binary_Tree<B> :: DeleteNode(Node<B>* root, B k)
+{
+    if(root == NULL) {return NULL;}
+    if(root->left == NULL && root->right == NULL)
+    {
+        if(root->data == k) {return NULL;}
+        else {return root;}
+    }
+    Node<B>* key_node = NULL;
+    Node<B>* temp;
+    Node<B>* last;
+    queue<Node<B>*> q;
+    q.push(root);
+    // Do level order traversal to find deepest
+    // node(temp), node to be deleted (key_node)
+      // and parent of deepest node(last)
+    while(!q.empty())
+    {
+        temp = q.front();
+        q.pop();
+        if(temp->data == k) {key_node = temp;}
+        if(temp->left)
+        {
+            last = temp;//storing the parent node
+            q.push(temp->left);
+        }
+        if(temp->right)
+        {
+            last = temp;// storing the parent node
+            q.push(temp->right);
+        }
+    }
+    if(key_node != NULL)
+    {
+        key_node->data = temp->data;//replacing key_node's data to deepest node's data
+        if(last->right == temp) {last->right = NULL;}
+        else {last->left = NULL;}
+        delete(temp);
+    }
+    return root;
+}
+
+template<typename B>
+Node<B>* Binary_Tree<B> :: findMin(Node<B>* root)
+{
+	Node<B>* c = root;
+	while(c && c->left != NULL){c = c->left;}
+	return c;
+}
+
+template<typename B>
+void Binary_Tree<B> :: InorderRec(Node<B>* t)
+{
+    if(t != NULL)
+    {
+        InorderRec(t->left);
+        cout<<t->data<<" ";
+        InorderRec(t->right);
+    }
+}
+
+template<typename B>
+void Binary_Tree<B> :: PreorderRec(Node<B>* t)
+{
+    if(t != NULL)
+    {
+        cout<<t->data<<" ";
+        PreorderRec(t->left);
+        PreorderRec(t->right);
+    }
+}
+
+template<typename B>
+void Binary_Tree<B> :: PostorderRec(Node<B>* t)
+{
+    if(t != NULL)
+    {
+        PostorderRec(t->left);
+        PostorderRec(t->right);
+        cout<<t->data<<" ";
+    }
+}
+
+template<typename B>
+int Binary_Tree<B> :: Height(Node<B>* root)
+{
+    if(root == NULL)
+    {
+        return 0;
+    }
+    else
+    {
+        return 1 + max(Height(root->left), Height(root->right));
+    }
+}
+
+template<typename B>
+void Binary_Tree<B> :: MirrorImage(Node<B>* root)
+{
+    if(root != NULL)
+    {
+        struct Node<B>* temp;
+        MirrorImage(root->left);
+        MirrorImage(root->right);
+        temp = root->left;
+        root->left = root->right;
+        root->right = temp;
+    }
+}
+
+template<typename B>
+void Binary_Tree<B> :: copyTree(Node<B>* tree2, Node<B>* tree)
+{
+    if(tree == NULL)
+    {
+        tree2 = NULL;
+    }
+    else
+    {
+        tree2 = new Node<B>;
+        tree2->data = tree->data;
+        copyTree(tree2->left, tree->left);
+        cout<<tree2->data<<" ";
+        copyTree(tree2->right, tree->right);
+    }
+}
+
+template<typename B>
+void Binary_Tree<B> :: DisplayLeafNodes(Node<B>* root)
+{
+    if(!root)
+    {
+        cout<<"\nTree is Empty";
+        return;
+    }
+    if(root->left == NULL && root->right == NULL)
+    {
+        cout<<root->data<<" ";
+        return;
+    }
+    if(root->left)
+       DisplayLeafNodes(root->left);
+    if(root->right)
+       DisplayLeafNodes(root->right);
+}
+
+template<typename B>
+void Binary_Tree<B> :: DisplayLevelWise(Node<B>* root)
+{
+    queue<Node<B>*> Q;
+    Q.push(root);
+    while(!Q.empty())
+    {
+        Node<B>* temp = Q.front();
+        Q.pop();
+        cout<<"\n\t\t\t"<<temp->data<<endl;
+        if(temp->left) {Q.push(temp->left);}
+        if(temp->right) {Q.push(temp->right);}
+    }
+}
+#endif
